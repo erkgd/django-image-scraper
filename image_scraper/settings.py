442d611a1936ccd8a92733e190dd068d0626f7ed
erkgd <alejandrogarrancho@gmail.com>
2025-05-20 17:53:50 +0200

    Remove unused Tailwind and theme apps from settings

--- image_scraper/settings.py
+++ image_scraper/settings.py
@@ -21,8 +21,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tailwind',
-    'theme',
     'scraper',
     'rest_framework',  # Added DRF for API endpoints
 ]
